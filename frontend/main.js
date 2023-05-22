e5d3da3f35a547b6e65f17f53d70c8e7a9b2d7b1
Ladcze <orimsway2cool@yahoo.com>
2023-05-23 00:25:53 +0100

    refresh

--- frontend/main.js
+++ frontend/main.js
@@ -1,11 +1,12 @@
 window.addEventListener('DOMContentLoaded', (event) =>{ 
     getVisitCount();
 })
-    const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = "https://w2cazureresume.azurewebsites.net/api/GetResumeCounter?code=80BNNa_pj_YMc6zcTsXZ6jnzMK7W6kax7vZZ9zKhjvAyAzFuF90p2w==https://w2cazureresume.azurewebsites.net/api/GetResumeCounter?code=80BNNa_pj_YMc6zcTsXZ6jnzMK7W6kax7vZZ9zKhjvAyAzFuF90p2w==";
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response => {
         console.log("website called function API");
